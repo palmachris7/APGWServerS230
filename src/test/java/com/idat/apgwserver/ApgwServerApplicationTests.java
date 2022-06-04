822c5c1e14c6cf0a330ca1b3733bb7a59f7de933
palmachris7 <palmachristopher7@hotmail.com>
2022-06-03 21:57:53 -0500

    Initial commit

---
+++ src/test/java/com/idat/apgwserver/ApgwServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.apgwserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgwServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
